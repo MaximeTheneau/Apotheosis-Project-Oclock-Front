d073bd3a2172e56aa2f14059f2c01bd6b90ba9c7
LoicMAURIN <loic.maurin123@orange.fr>
2022-07-20 16:40:51 +0200

    Setting up card and icons for Home

--- src/components/Main/Home/index.js
+++ src/components/Main/Home/index.js
@@ -5,11 +5,15 @@ function Home() {
     <div className="home">
       <form>
         <input type="text" />
-        <button type="submit">Rechercher </button>
+        <button type="submit">
+          <i className="icon-search" />
+        </button>
       </form>
       <ul className="home-list-categories">
-        <li><i className="icon-radish" /></li>
-        <li><i className="icon-dish" /></li>
+        <li className="home-list-categories-icon"><i className="icon-drink" /></li>
+        <li className="home-list-categories-icon"><i className="icon-radish" /></li>
+        <li className="home-list-categories-icon"><i className="icon-dish" /></li>
+        <li className="home-list-categories-icon"><i className="icon-cakes" /></li>
       </ul>
 
       <h2 className="home-recipe">Les Recettes Miam</h2>
@@ -22,9 +26,14 @@ function Home() {
             alt="Name"
             className="home-card-img"
           />
-          <ul className="home-card-icon">
-            <li><i className="icon-radish" /></li>
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
             <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
           </ul>
         </div>
 
@@ -34,9 +43,93 @@ function Home() {
             alt="Name2"
             className="home-card-img"
           />
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
+            <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="home-card">
+          <img
+            src="https://thumbs.dreamstime.com/b/ligne-horizontale-de-la-mer-par-plage-vue-panoramique-horizontal-202984827.jpg"
+            alt="Name2"
+            className="home-card-img"
+          />
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
+            <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
+          </ul>
         </div>
 
       </div>
+
+      <h2>Les Dernières Recettes </h2>
+
+      <div className="home-cards">
+
+        <div className="home-card">
+          <img
+            src="https://images.pexels.com/photos/53483/strawberries-crepe-dessert-sweet-53483.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt="Name"
+            className="home-card-img"
+          />
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
+            <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="home-card">
+          <img
+            src="https://images.pexels.com/photos/263041/pexels-photo-263041.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt="Name2"
+            className="home-card-img"
+          />
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
+            <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="home-card">
+          <img
+            src="https://thumbs.dreamstime.com/b/ligne-horizontale-de-la-mer-par-plage-vue-panoramique-horizontal-202984827.jpg"
+            alt="Name2"
+            className="home-card-img"
+          />
+          <ul className="home-card-list">
+            <li><img className="home-card-list-img-user" src="https://image.shutterstock.com/image-photo/carer-pushing-senior-woman-wheelchair-260nw-1148689052.jpg" alt="zz" /></li>
+            <li><i className="icon-dish" /></li>
+            <li>
+              <span>15
+                <i className="icon-miam" />
+              </span>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
     </div>
   );
 }
