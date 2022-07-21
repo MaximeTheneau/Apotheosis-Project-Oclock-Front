b7596eb8ba436d745991a17a1de83929279795fa
LoicMAURIN <loic.maurin123@orange.fr>
2022-07-21 11:58:14 +0200

    Setting up routes in the footer and BrowserRouter in the major index

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Aboutus from '../Pages/Aboutus';
 import Tscs from '../Pages/Tscs';
@@ -9,50 +10,59 @@ import './styles.scss';
 
 function Footer() {
   return (
-    <nav className="footer">
-      <ul className="footer-list">
-        <li className="footer-list-aboutus">
-          <NavLink
-            className={
-              ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
-          }
-          to="/Aboutus"
-          >
-            Qui sommes nous ?
-          </NavLink>
-        </li>
-        <li className="footer-list-contact">
-          <NavLink
-            className={
-              ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
-          }
-          to="/Contact"
-          >
-            Contact
-          </NavLink>
-        </li>
-        <li className="footer-list-privacy">
-          <NavLink
-            className={
-              ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
-          }
-          to="/Privacy"
-          >
-            Mentions légal
-          </NavLink>
-        </li>
-        <li className="footer-list-tscs">
-          <NavLink
-            className={
-              ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
-          }
-          to="/Tscs"
-          >
-            Conditions général d'utilisation
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="routes-footer">
+      <Routes>
+        <Route path="/qui-sommes-nous" element={<Aboutus />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mention-legal" element={<Privacy />} />
+        <Route path="/condition-general-utilisation" element={<Tscs />} />
+      </Routes>
+
+      <nav className="footer">
+        <ul className="footer-list">
+          <li className="footer-list-aboutus">
+            <NavLink
+              className={
+                ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
+            }
+              to="/qui-sommes-nous"
+            >
+              Qui sommes nous ?
+            </NavLink>
+          </li>
+          <li className="footer-list-contact">
+            <NavLink
+              className={
+                ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
+            }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li className="footer-list-privacy">
+            <NavLink
+              className={
+                ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
+            }
+              to="/mention-legal"
+            >
+              Mentions légal
+            </NavLink>
+          </li>
+          <li className="footer-list-tscs">
+            <NavLink
+              className={
+                ({ isActive }) => (isActive ? 'footer-link footer-link--active' : 'footer-link')
+            }
+              to="/condition-general-utilisation"
+            >
+              Conditions général d'utilisation
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import Omiam from 'src/components/Omiam';
 
-const rootReactElement = <Omiam />;
+const rootReactElement = (
+  <BrowserRouter>
+    <Omiam />
+  </BrowserRouter>
+);
 
 const root = createRoot(document.getElementById('root'));
-
 root.render(rootReactElement);
