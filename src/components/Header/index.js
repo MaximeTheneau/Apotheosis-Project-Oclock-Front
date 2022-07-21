9ede7c365d48816ed1e0a07042d2c03faf309062
paulinethomelin <pauline.thomelin@gmail.com>
2022-07-21 12:35:24 +0200

    Button login added for the mobile version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,20 +2,26 @@ import './styles.scss';
 
 function Header() {
   return (
-    <header className="header">
-      <div className="header-logo">
-        <h2 className="header-logo-title">
-          <i className="icon-miam" />'miam
-        </h2>
-        <span className="header-logo-sloggan">Une histoire de <em className="header-logo-sloggan-highlight">miam's</em></span>
-      </div>
-      <div className="header-menu">
-        <button type="submit">
-          <i className="icon-bars" />
+    <header>
+      <div className="connexion">
+        <button className="connexion-button" type="submit">Connexion <i className="icon-lock header-menu-logo" />
         </button>
+      </div>
+      <div className="header">
+        <div className="header-logo">
+          <h2 className="header-logo-title">
+            <i className="icon-miam" />'miam
+          </h2>
+          <span className="header-logo-sloggan">Une histoire de <em className="header-logo-sloggan-highlight">miam's</em></span>
+        </div>
+        <div className="header-menu">
+          <button type="submit">
+            <i className="icon-bars" />
+          </button>
+        </div>
       </div>
     </header>
   );
 }
 
-export default Header; 
+export default Header;
