d9e290f8f3c002cccada2ad9e89aeb19eacd7964
LoicMAURIN <loic.maurin123@orange.fr>
2022-07-22 12:22:01 +0200

    added value and scss

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -5,19 +5,21 @@ function Contact() {
     <div className="contact">
       <h1>Contactez-nous</h1>
       <p>Une remarque ? Une suggestion ? N'hesitez-pas à nous écrire.</p>
-      <form className="form-pseudo">
-        <input className="pseudo-input" type="text" placeholder="Votre Pseudo" />
+      <form className="form-general">
+        <form className="form-pseudo">
+          <input className="pseudo-input" type="text" placeholder="Votre Pseudo" value="" />
+        </form>
+        <form className="form-email">
+          <input className="email-input" type="email" placeholder="Votre E-mail" value="" />
+        </form>
+        <form className="form-sujet">
+          <input className="sujet-input" type="text" placeholder="Sujet" value="" />
+        </form>
+        <form className="form-message">
+          <input className="message-input" type="text" placeholder="Votre Message" value="" />
+        </form>
+        <button className="send-button" type="submit">Envoyer</button>
       </form>
-      <form className="form-email">
-        <input className="email-input" type="text" placeholder="Votre E-mail" />
-      </form>
-      <form className="form-sujet">
-        <input className="sujet-input" type="text" placeholder="Sujet" />
-      </form>
-      <form className="form-message">
-        <input className="message-input" type="text" placeholder="Votre Message" />
-      </form>
-      <button className="send-button" type="submit">Envoyer</button>
     </div>
   );
 }
