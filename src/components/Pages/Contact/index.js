2e8a9657a58e73f015cce5fecada648aceb90b1e
LoicMAURIN <loic.maurin123@orange.fr>
2022-07-22 17:17:46 +0200

    Change form by label and change scss add id input in page Contact

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -19,42 +19,46 @@ function Contact() {
       <h1>Contactez-nous</h1>
       <p>Une remarque ? Une suggestion ? N'hesitez-pas à nous écrire.</p>
       <form className="form-general" onSubmit={handleSubmit}>
-        <form className="form-pseudo">
+        <label htmlFor="label-pseudo">
           <input
             className="pseudo-input"
             type="text"
+            id="pseudo"
             placeholder="Votre Pseudo"
             value={inputValue}
             onChange={handleOnChange}
           />
-        </form>
-        <form className="form-email">
+        </label>
+        <label htmlFor="label-email">
           <input
             className="email-input"
             type="email"
+            id="email"
             placeholder="Votre E-mail"
             value={inputValue}
             onChange={handleOnChange}
           />
-        </form>
-        <form className="form-sujet">
+        </label>
+        <label htmlFor="label-sujet">
           <input
             className="topic-input"
             type="text"
+            id="topic"
             placeholder="Sujet"
             value={inputValue}
             onChange={handleOnChange}
           />
-        </form>
-        <form className="form-message">
+        </label>
+        <label htmlFor="label-message">
           <input
             className="message-input"
             type="text"
+            id="message"
             placeholder="Votre Message"
             value={inputValue}
             onChange={handleOnChange}
           />
-        </form>
+        </label>
         <button className="send-button" type="submit">Envoyer</button>
       </form>
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducer from 'src/reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(),
 );
 
 export default store;
