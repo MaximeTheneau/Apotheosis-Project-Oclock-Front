f66fa9148275d5394e98a0d0de7153b90cc76170
paulinethomelin <pauline.thomelin@gmail.com>
2022-07-25 19:20:04 +0200

    Setting up of the Register component, scss + reducer

--- src/components/Connectionform/index.js
+++ src/components/Connectionform/index.js
@@ -1,9 +1,15 @@
 import './styles.scss';
+import { Link } from 'react-router-dom';
+import Register from '../Register';
 
 function Connectionform() {
   return (
     <div className="form">
-      <p>Hello</p>
+      <div className="form-title">
+        <Link to="/login"><h2 className="form-h2">Se connecter</h2></Link>
+        <Link to="/login"><h2 className="form-h2">Créer un compte</h2></Link>
+      </div>
+      <Register />
     </div>
   );
 }
---
+++ src/components/Register/index.js
@@ -0,0 +1,83 @@
+import './styles.scss';
+import { useSelector, useDispatch } from 'react-redux';
+
+function Register() {
+  const {
+    isRegister, pseudo, email, password, confirmedPassword,
+  } = useSelector((state) => state.header.settingsRegister);
+  return (
+    <form className="registration">
+      <div className="registration-field">
+        <label
+          htmlFor="pseudo"
+          className="registration-label"
+        >
+          Pseudo *
+          <input
+            placeholder="Chef O'miam"
+            type="pseudo"
+            id="pseudo"
+            value={pseudo}
+            required
+            className="registration-input"
+            size="28"
+          />
+        </label>
+      </div>
+      <div className="registration-field">
+        <label
+          htmlFor="email"
+          className="registration-label"
+        >
+          Email *
+          <input
+            placeholder="email@omiam.com"
+            type="email"
+            id="email"
+            value={email}
+            required
+            className="registration-input"
+            size="28"
+          />
+        </label>
+      </div>
+      <div className="registration-field">
+        <label
+          htmlFor="password"
+          className="registration-label"
+        >
+          Mot de passe *
+          <input
+            placeholder="*****"
+            type="password"
+            id="password"
+            value={password}
+            required
+            className="registration-input"
+            size="28"
+          />
+        </label>
+      </div>
+      <div className="registration-field">
+        <label
+          htmlFor="confirmedpassword"
+          className="registration-label"
+        >
+          Confirmation du mot de passe *
+          <input
+            placeholder="*****"
+            type="confirmedpassword"
+            id="confirmedpassword"
+            value={confirmedPassword}
+            required
+            className="registration-input"
+            size="28"
+          />
+        </label>
+      </div>
+      <button type="submit" className="registration-submit">Valider</button>
+    </form>
+  );
+}
+
+export default Register;
