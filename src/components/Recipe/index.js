85b9897927122118863cc8da99d2605f10590192
LoicMAURIN <loic.maurin123@orange.fr>
2022-07-29 11:35:20 +0200

    Setting up caption and steps for Recipe

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -15,6 +15,23 @@ function Recipe() {
           className="one-recipe-img"
         />
       </div>
+      <div className="one-recipe-timer">
+        <ul className="one-recipe-timer-ul">
+          <li><i className="icon-timer" /><span>15 min</span></li>
+          <li><i className="icon-kitchen-hat" /></li>
+          <li><p className="one-recipe-caption">
+            Découvrez la recette du Mojito, la star des cocktails de l'été grâce à
+            sa menthe parfumée et son irrésistible fraîcheur.
+              </p>
+          </li>
+        </ul>
+        <div className="one-recipe-steps">
+          <ul className="one-recipe-steps-ul">
+            <li><h2 className="one-recipe-steps-title">Etape 1</h2></li>
+            <li><p className="one-recipe-steps-etape">Faites un puits au milieu et versez-y les œufs.</p></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
