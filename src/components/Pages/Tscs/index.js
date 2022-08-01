9aa330a1a12a21ea9a77cea42d1d915c86f1f0ae
NathalieTournon <ntournon@gmail.com>
2022-08-01 18:38:06 +0200

    TsCs first try

--- src/components/Pages/Tscs/index.js
+++ src/components/Pages/Tscs/index.js
@@ -2,7 +2,173 @@ import './styles.scss';
 
 function TsCs() {
   return (
-    <p>TsCs</p>
+    <div>
+      <h1>CONDITIONS GÉNÉRALES D'UTILISATION DU SITE O'MIAM</h1>
+
+      <h2>ARTICLE 1 : Objet</h2>
+      <p>Les présentes « Conditions Générales d'Utilisation » ont pour objet</p>
+      <p>l'encadrement juridique de l’utilisation du site O’Miam et de ses services.</p>
+
+      <p>Ce contrat est conclu entre :</p>
+      <p>Le gérant du site internet, ci-après désigné « l’Éditeur »,</p>
+      <p>Toute personne physique ou morale souhaitant accéder au site et à ses services</p>
+      <p>ci-après appelé « l’Utilisateur ».</p>
+
+      <p>Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur,</p>
+      <p> et son accès au site vaut acceptation de ces conditions.</p>
+
+      <h2>ARTICLE 2 : Mentions Légales</h2>
+
+      <p>Le site O’Miam est édité par la <strong>Team Curie - Projet O’Miam</strong></p>
+      <p> domiciliée sur le Téléporteur de l’école O’Clock.</p>
+
+      <h2>ARTICLE 3 : Accès aux Services</h2>
+      <div>
+        <ul>L’Utilisateur du site O’Miam a accès aux services suivants :
+          <li>Création d’un compte utilisateur</li>
+          <li>Visualisation des recettes publiées par les membres enregistrés</li>
+          <li>Visualisation des dernières recettes publiées par les Membres</li>
+          <li>Visualisation des recettes les plus miamées par les Membres</li>
+          <li>Faire une recherche par mot-clé</li>
+          <li>Contacter l'équipe d'O'Miam</li>
+          <li>Accéder aux Mentions Légales du site</li>
+          <li>Accéder aux CGU du site</li>
+        </ul>
+
+        <p>Tout utilisateur ayant accès à internet peut accéder gratuitement<br />
+          et depuis n’importe où au site.
+        </p>
+        <p>Les frais supportés par l’Utilisateur pour y accéder <br />
+          (connexion internet, matériel informatique, etc.)
+        </p>
+        <p>ne sont pas à la charge de l’Éditeur.</p>
+        <ul>Les services suivants ne sont accessibles pour l’Utilisateur <br />
+          que s’il est membre du site(c’est-à-dire s’il est identifié<br />
+          à l’aide de ses identifiants de connexion) :
+          <li>Se connecter et se déconnecter de son compte</li>
+          <li>Publication puis édition et suppression de ses recettes</li>
+          <li>Possibilité d'attribuer ou de retirer un miam à des recettes</li>
+          <li>Possibilité de se créer un carnet de miams</li>
+          <li>Publication puis édition ou suppression de commentaires laissés sur les recettes</li>
+          <li>Modification ou Suppression de son compte</li>
+        </ul>
+
+        <p>Le site et ses différents services peuvent être interrompus ou suspendus<br />
+          par l’Éditeur notamment à l’occasion d’une maintenance,<br />
+          sans obligation de préavis ou de justification.
+        </p>
+      </div>
+
+      <h2>ARTICLE 4 : Responsabilité de l'Utilisateur</h2>
+      <div>
+        <p>L'Utilisateur est responsable des risques liés à l’utilisation <br />
+          de son identifiant de connexion et de son mot de passe. <br />
+
+          Le mot de passe de l’Utilisateur doit rester secret.<br />
+          En cas de divulgation de mot de passe, l’Éditeur décline toute responsabilité.<br />
+
+          L’Utilisateur assume l’entière responsabilité de l’utilisation <br />
+          qu’il fait des informations et contenus présents sur le site O’Miam.<br />
+
+          Tout usage du service par l'Utilisateur ayant directement ou indirectement<br />
+          pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+        </p>
+
+        <ul>Le site permet aux membres de publier :
+          <li>des Recettes</li>
+          <li>des Commentaires</li>
+          <li>des Photos</li>
+        </ul>
+
+        <p>Le membre s’engage à tenir des propos respectueux des autres et de la loi <br />
+          et accepte que ces publications soient modérées ou refusées par l’Éditeur,<br />
+          sans obligation de justification. <br />
+          En publiant sur le site, l’Utilisateur cède à la société éditrice <br />
+          le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier,<br />
+          diffuser et distribuer sa publication, directement ou par un tiers autorisé.<br />
+          L’Éditeur s'engage toutefois à citer le membre en cas d’utilisation de sa publication
+        </p>
+      </div>
+
+      <h2>ARTICLE 5 : Responsabilité de l'Editeur</h2>
+      <div>
+        <p>Tout dysfonctionnement du serveur ou du réseau ne peut engager<br />
+          la responsabilité de l’Éditeur.<br />
+          De même, la responsabilité du site ne peut être engagée en cas <br />
+          de force majeure ou du fait imprévisible et insurmontable d'un tiers.<br />
+          Le site O’Miam s'engage à mettre en œuvre tous les moyens nécessaires <br />
+          pour garantir la sécurité et la confidentialité des données.<br />
+          Toutefois, il n’apporte pas une garantie de sécurité totale.<br />
+          L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources,<br />
+          bien que les informations diffusées sur le site soient réputées fiables.
+        </p>
+      </div>
+
+      <h2>ARTICLE 6 : Proprièté Intellectuelle</h2>
+      <div>
+        <p>Les contenus du site O’Miam (logos, textes, éléments graphiques, vidéos, etc.)<br />
+          sont protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle<br />
+          L’Utilisateur devra obtenir l’autorisation de l’éditeur du site <br />
+          avant toute reproduction, copie ou publication de ces différents contenus.<br />
+          Ces derniers peuvent être utilisés par les utilisateurs à des fins privées;<br />
+          tout usage commercial est interdit.<br />
+          L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne<br />
+          et il s’engage à ne pas porter atteinte à un tiers.<br />
+          L’Éditeur du site se réserve le droit de modérer ou de supprimer librement <br />
+          et à tout moment les contenus mis en ligne par les utilisateurs, et ce sans justification.
+        </p>
+      </div>
+
+      <h2>ARTICLE 7 : Données Personnelles</h2>
+      <div>
+        <p>L’Utilisateur doit obligatoirement fournir des informations personnelles <br />
+          pour procéder à son inscription sur le site. <br />
+          L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée <br />
+          par le site O’Miam pour la communication d’informations diverses <br />
+          et la gestion du compte.<br />
+          O’Miam garantit le respect de la vie privée de l’utilisateur,<br />
+          conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique,<br />
+          aux fichiers et aux libertés.<br />
+          En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978,<br />
+          l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression <br />
+          et d'opposition de ses données personnelles.<br />
+          L'Utilisateur exerce ce droit via un formulaire de contact.
+        </p>
+      </div>
+
+      <h2>ARTICLE 8 : Liens Hypertextes</h2>
+      <div>
+        <p>Les domaines vers lesquels mènent les liens hypertextes présents sur le site<br />
+          n’engagent pas la responsabilité de l’Éditeur de O’Miam,<br />
+          qui n’a pas de contrôle sur ces liens.<br />
+          Il est possible pour un tiers de créer un lien vers une page du site O’Miam<br />
+          sans autorisation expresse de l’éditeur.
+        </p>
+      </div>
+
+      <h2>ARTICLE 9 : Évolution des conditions générales d’utilisation</h2>
+      <div>
+        <p>Le site O’Miam se réserve le droit de modifier les clauses <br />
+          de ces conditions générales d’utilisation à tout moment et sans justification.
+        </p>
+      </div>
+
+      <h2>ARTICLE 10 : Durée du Contrat</h2>
+      <div>
+        <p>La durée du présent contrat est indéterminée.<br />
+          Le contrat produit ses effets à l'égard de l'Utilisateur<br />
+          à compter du début de l’utilisation du service.
+        </p>
+      </div>
+
+      <h2>ARTICLE 11 : Droit applicable et juridiction compétente</h2>
+      <div>
+        <p>Le présent contrat dépend de la législation française. <br />
+          En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur,<br />
+          les tribunaux d’O’Clock sont compétents pour régler le contentieux.
+        </p>
+      </div>
+    </div>
   );
 }
 
